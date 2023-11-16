82c9eec0b8ba1a320198fb7e041ec588cd53288f
DONSAN7 <148339936+DONSAN7@users.noreply.github.com>
2023-11-16 02:30:12 +0000

    [Update]bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,15 +18,23 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    user = User.find(current_user.id)
+    user.update(user_params)
+    redirect_to user_path(current_user.id)
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :image, :introduction)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about'
 
-  resources :users, only: [:create, :index, :show, :edit]
-  resources :books, only: [:new, :create, :index, :show, :edit]
+  resources :users, only: [:new, :create, :index, :show, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update,:destroy]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
